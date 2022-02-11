5b0fd408f175084242816863024f5ccdfc43a6fe
Dell <mrterkmglsv@gmail.com>
2022-02-11 17:05:27 +0100

    sss

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -18,6 +18,6 @@ public class Runner {
     // Runner classının bodysine hiçbir kod yazmıyoruz.
     // Bu class için önemli olan kullnacagımız iki adet notasyon var.
 
-
+ // merhaba seyfullah hoca bende Murat
 
 }
