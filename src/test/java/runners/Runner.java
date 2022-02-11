a9cd7d7aacd0a07b36da33dc498771ece7100fee
seyfullah merdan <merdanseyfullah33@gmail.com>
2022-02-11 10:35:03 +0100

    cucumber

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,23 @@
+package runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features="src/test/resources/features",
+        glue= "src/test/java/stepdefinitions",
+        tags="@wip",
+        dryRun = false
+)
+
+public class Runner {
+
+    // Runner classının bodysine hiçbir kod yazmıyoruz.
+    // Bu class için önemli olan kullnacagımız iki adet notasyon var.
+
+
+
+}
---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,69 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.time.Duration;
+
+public class Driver {
+
+    private Driver() {    // Bu üyelere başka classlardan ulaşabilmm için
+                 // getter setter yapmam gerekiyor. Getter Setter yapmadıgım sürece burdaki private ögelere ulaşamam.
+    }
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver(){
+
+        if (driver==null) { // bu if sayesinde kod çalışırken bir kere new keyword ile driver oluşturulacak
+                             // diger kullanımlarda devreye girmeyecek.
+            switch (ConfigReader.getProperty("browser")) {
+                case "chrome" :
+                    WebDriverManager.chromedriver().setup();
+                    driver=new ChromeDriver();
+                    break;
+                case "firefox" :
+                    WebDriverManager.firefoxdriver().setup();
+                    driver=new FirefoxDriver();
+                    break;
+                case "opera" :
+                    WebDriverManager.operadriver().setup();
+                    driver=new OperaDriver();
+                    break;
+                case "safari" :
+                    WebDriverManager.safaridriver().setup();
+                    driver=new SafariDriver();
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+            }
+
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        return driver;
+    }
+// Bu methodu her çagırdıgımda yeni bir driver çagırıyor. Tekrardan oluşturuyor.
+
+
+    public static void closeDriver() {
+
+        if (driver!=null) {
+            driver.quit();
+            driver=null; // null ataması yapmazsam açılırken nulla eşit olmadıgı için yeni sayfa açmaz ve hata verir
+            // kapattıktan sonra driveri sıfırlıyorum ki bir daha çagırdıgımda nulla takılsın ve setup methodunda driveri
+            // çagırsın ve çalıştırsın
+        }
+    }
+
+
+
+
+}
